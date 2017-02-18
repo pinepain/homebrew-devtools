6deaabbf4c936b5763502cbe7ad8ec1986cb5680
Bogdan Padalko <pinepain@gmail.com>
2017-02-18 17:48:07 +0200

    php-v8 0.1.2

--- Formula/php70-v8.rb
+++ Formula/php70-v8.rb
@@ -2,22 +2,21 @@ require Formula['php70'].path.parent.parent + "Abstract/abstract-php-extension"
 
 class Php70V8 < AbstractPhp71Extension
   init
-  desc "PHP extension for V8-5.7.92 JavaScript engine"
+  desc "PHP extension for V8 JavaScript engine"
   homepage "https://github.com/pinepain/php-v8"
-  url "https://github.com/pinepain/php-v8/archive/v0.1.0.tar.gz"
-  sha256 "b203da5b7fc72ef0cd71af26b409e2a1977c7e9a1c48648b33882c325ab755a3"
+  url "https://github.com/pinepain/php-v8/archive/v0.1.2.tar.gz"
+  sha256 "2d041014ccb1437d694a9070cfa5500bdf349bcfcf922db1e6aef13ff855f91c"
   head "https://github.com/pinepain/php-v8.git"
 
-  depends_on 'v8@5.7'
+  depends_on 'v8@5.82'
 
   bottle do
   end
 
-
   def install
     ENV.universal_binary if build.universal?
 
-    v8_prefix=Formula['v8@5.7'].opt_prefix
+    v8_prefix=Formula['v8@5.8'].opt_prefix
 
     safe_phpize
     system "./configure", "--prefix=#{prefix}", "--with-v8=#{v8_prefix}", phpconfig
