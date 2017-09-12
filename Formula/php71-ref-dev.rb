e167e742280c14b374f89a3ecb5c0853a8e196f4
Bogdan Padalko <pinepain@gmail.com>
2017-09-12 22:45:53 +0300

    Simplify building bottles [skip ci]

--- Formula/php71-ref-dev.rb
+++ Formula/php71-ref-dev.rb
@@ -1,4 +1,4 @@
-require Formula['php72'].path.parent.parent + "Abstract/abstract-php-extension"
+require Formula['php71'].path.parent.parent + "Abstract/abstract-php-extension"
 
 class Php71RefDev < AbstractPhp71Extension
   init
@@ -7,6 +7,12 @@ class Php71RefDev < AbstractPhp71Extension
   url "https://github.com/pinepain/php-ref/archive/v0.5.0.tar.gz"
   sha256 "0fd928fd8314f836a97e3833d6c5e15658202d05fe3a0725d793f6e06394cd97"
   head "https://github.com/pinepain/php-ref.git"
+
+  bottle do
+    root_url "https://dl.bintray.com/pinepain/bottles-devtools"
+    cellar :any_skip_relocation
+    sha256 "237ffec7c5943b9dcd1e6264346f31a8ce6b3fa388493b5eacd5734114dc8584" => :sierra
+  end
 
   def install
     safe_phpize
