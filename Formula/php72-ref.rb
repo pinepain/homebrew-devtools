a780d249d78e9a311b67cdf5215d76178d8066a7
Bogdan Padalko <pinepain@gmail.com>
2017-09-16 16:08:43 +0300

    build php72-ref

--- Formula/php72-ref.rb
+++ Formula/php72-ref.rb
@@ -1,6 +1,6 @@
 require Formula['php72'].path.parent.parent + "Abstract/abstract-php-extension"
 
-class Php72RefDev < AbstractPhp72Extension
+class Php72Ref < AbstractPhp72Extension
   init
   desc "Soft and Weak references support for PHP"
   homepage "https://github.com/pinepain/php-ref"
