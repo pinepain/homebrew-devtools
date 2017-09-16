180edfda48335cdd5df6b9bd21a2a653417196a7
Bogdan Padalko (Travis CI) <pinepain@gmail.com>
2017-09-16 12:40:35 +0000

    Build and deploy php71-ref source package [skip ci]

--- Formula/php71-ref.rb
+++ Formula/php71-ref.rb
@@ -8,6 +8,12 @@ class Php71Ref < AbstractPhp71Extension
   sha256 "0fd928fd8314f836a97e3833d6c5e15658202d05fe3a0725d793f6e06394cd97"
   head "https://github.com/pinepain/php-ref.git"
 
+  bottle do
+    root_url "https://dl.bintray.com/pinepain/bottles-devtools"
+    cellar :any_skip_relocation
+    sha256 "ddc60e5f87bcba939517312c50e6d039f923cd6aafb031c3d6058d51ca7a5212" => :sierra
+  end
+
   def install
     safe_phpize
     system "./configure", "--prefix=#{prefix}", phpconfig
