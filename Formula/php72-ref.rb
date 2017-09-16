5f59011cd0a5cc7ee50fd082a56ed7636317fd5d
Bogdan Padalko (Travis CI) <pinepain@gmail.com>
2017-09-16 13:27:35 +0000

    Build and deploy php72-ref source package [skip ci]

--- Formula/php72-ref.rb
+++ Formula/php72-ref.rb
@@ -8,6 +8,12 @@ class Php72Ref < AbstractPhp72Extension
   sha256 "0fd928fd8314f836a97e3833d6c5e15658202d05fe3a0725d793f6e06394cd97"
   head "https://github.com/pinepain/php-ref.git"
 
+  bottle do
+    root_url "https://dl.bintray.com/pinepain/bottles-devtools"
+    cellar :any_skip_relocation
+    sha256 "8520ac0b25fc0dfdfb1994d7cbc39ba7c388ac7ae62f54165adc0657ae53dc52" => :sierra
+  end
+
   def install
     safe_phpize
     system "./configure", "--prefix=#{prefix}", phpconfig
