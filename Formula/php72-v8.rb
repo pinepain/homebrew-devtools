e3e784738b2038af42f1ee8a5bee3c1007162756
Bogdan Padalko <pinepain@gmail.com>
2018-02-28 00:03:22 +0200

    Build and deploy php72-v8 source package [skip ci]

--- Formula/php72-v8.rb
+++ Formula/php72-v8.rb
@@ -4,19 +4,19 @@ class Php72V8 < AbstractPhp72Extension
   init
   desc "PHP extension for V8 JavaScript engine"
   homepage "https://github.com/pinepain/php-v8"
-  url "https://github.com/pinepain/php-v8/archive/v0.2.1.tar.gz"
-  sha256 "b06b8b266c753ebc9c41f59f86474bb9b90eeb711da6d2abc7a47e7ba5f57f0b"
+  url "https://github.com/pinepain/php-v8/archive/v0.2.2.tar.gz"
+  sha256 "6c8cf2fa57ba45ccf19b238114cb3b0dec2ad7d8533f5ba6d8a809dd3482eb02"
   head "https://github.com/pinepain/php-v8.git"
 
-  depends_on 'v8@6.4'
+  depends_on 'v8@6.6'
 
   bottle do
     root_url "https://dl.bintray.com/pinepain/bottles-devtools"
-    sha256 "a7ab717d3b8da5c6c17234e364b75887556934d7d6970243db35789071a26c8b" => :high_sierra
+    sha256 "7f9051a6cd8226e7bc5b0b0db203901841a2bcae21341fcfe0f91b3364c89c99" => :high_sierra
   end
 
   def install
-    v8_prefix=Formula['v8@6.4'].opt_prefix
+    v8_prefix=Formula['v8@6.6'].opt_prefix
 
     safe_phpize
     system "./configure", "--prefix=#{prefix}", "--with-v8=#{v8_prefix}", phpconfig
